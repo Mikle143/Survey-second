8821d4504ff3c3d74a54e5b7de2c3701b43ecca7
Mikhail Vdovin <mvd143@mail.ru>
2024-09-12 14:08:28 +0300

    Исправил замечания к первой домашке

---
+++ common/src/test/java/ru/vdovmb/AppTest.java
@@ -0,0 +1,10 @@
+package ru.vdovmb;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void test() {
+        System.out.println("@Test");
+    }
+}
---
+++ common/src/test/java/ru/vdovmb/integration/ExampleIT.java
@@ -0,0 +1,11 @@
+package ru.vdovmb.integration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleIT {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
